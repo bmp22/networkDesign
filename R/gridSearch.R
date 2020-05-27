aae2ca0eacfa3d4ffe0613665a6ca8b4422183e7
Ben M. Parker <web.github.com@ben-parker.co.uk>
2020-05-27 17:22:43 +0100

    Commit on new desktop

--- R/gridSearch.R
+++ R/gridSearch.R
@@ -1,57 +1,68 @@
+# TODO- rewrite CE algorithm
 #' Search over all possible designs to find the global optimal
 #'
 #' Evaluates all possible designs by searching over a grid to find a global optimal. Can be slow for high A>20 or high p>2
 #' @param A An adjacency matrix
-#' @param p The number of treatments in the experiment,
+#' @param p The number of treatments in the experiment
 #' @param isomorphisms Whether to ignore designs that are isomorphic to designs evaluated earlier
 #' @param ignoreLastNode Whether we set the last node as zero. Can be useful to satisfy constraints in double blocking structures
 #' @param algorithm Which algorithm to use for finding which designs to evaluate; currently 'sequential', 'random' and 'ce' are implemented
 #' @param specialNodes Are there any nodes which are fixed that we don't want to apply treatments to.
-#' @keywords For a given adjacency matrix, find the optimal design with p treatemtnes
+#' @param networkEffects Should the optimal design for the network effects be found (default is to find direct effects)
+#' @keywords For a given adjacency matrix, find the optimal design with p treatments.
 #' @export
 #'
-gridSearch<-function(A,p,isomorphisms=FALSE,specialNodes=NULL, ignoreLastNode=FALSE, algorithm="sequential"){
-  n<-dim(A)[1]
-  b<-length(specialNodes)
-  trialCE<-2
-  numTrialsCE<-0
-  successfulCE<-1
-  numCERandStarts<-100 # How many random starts we do: TODO- put in parameters
-
+gridSearch<-function(A,p,isomorphisms=FALSE, blockList=NULL, ignoreLastNode=FALSE, algorithm="sequential",networkEffects=FALSE){
+  ### Initiate parameters
+  n<-dim(A)[1] # How many experimental units
+  b<-length(blockList) # How many blocks
   AOptVal<-Inf #
   AOptDesign<-NULL
-  AOptNetVal<-Inf #
-  AOptNetDesign<-NULL
-
-
   numEval<-0
-  z<- NULL
 
+  if (b>0) {specialNodes<-c((p+1):(p+b)) # For compatibility- delete later
+  }
+  else
+  {specialNodes<-NULL}
+
+  ## Make a copy of A, and expand, then add block structure to network
+  B<-matrix(0,ncol=(n+b),nrow=(n+b))
+  B[1:n,1:n]<-A
+  if (b>0){ #Assign nodes linked to blocks
+  for (i in 1:b){
+    B[blockList[[i]],n+i]<-1
+    B[n+i,blockList[[i]]]<-1
+  }
+  }
+  A<-B
+
+  #### Set parameters for coordinate exchange algorithm
+  trialCE<-2 # Start with the second node. Without loss of generality the first node's treatment can be fixed.
+  numTrialsCE<-0
+  lastCEWin<-0
+  numCERandStarts<-100 # How many random starts we do: TODO- put in parameters
+
+  ### Work out isomorphisms of the network if we are using them
+  z<- NULL
   if (isomorphisms==TRUE){
     ex1Igraph<-igraph::graph_from_adjacency_matrix(A)
-    z<-isomorphisms(ex1Igraph,ex1Igraph)
+    z<-isomorphisms(ex1Igraph,ex1Igraph) # Can this be faster in some other routine
     numIso<-length(z)
   }
   else{
     z[[1]]<-rep(1:n)
     numIso<-1
   }
+  zReduced<-lapply(z, function(z) utils::head(z, n))
 
-  zReduced<-lapply(z, function(z) utils::head(z, n-b))
-
+  ### For use in testing if a design is valid
   mult<-NULL
-  for (i in (1:(n-b))){
-    mult[i]<-p^((n-b)-i) #Todo: change for really big p
+  for (i in (1:(n))){
+    mult[i]<-p^((n)-i) #Todo: change for really big p
   }
 
-  lex.cmp <- function(vec1, vec2) {
-    index <- which.min(vec1 == vec2)  # find the first diff
-    sign(vec1[index] - vec2[index])   # assumes numeric
-  }
-
-
+# Thest whether a design is valid-
   testValidDesign<-function(td){
-
     x<-(match(c(1:p),td))
     if (anyNA(x)){return(FALSE)}
     if ((all(sort(x)==x))==FALSE){return(FALSE)} # Check whether design has elements 1:p with first
@@ -59,113 +70,110 @@ gridSearch<-function(A,p,isomorphisms=FALSE,specialNodes=NULL, ignoreLastNode=FA
     tdm<-sum(td*mult)
 
     for (i in (1:numIso)){
-      if(tdm>sum(td[zReduced[[i]]]*mult)){return(FALSE)}
+      if(tdm>sum(td[zReduced[[i]]]*mult)){return(FALSE)}  # Check whether design is  first in lexicographic order
     }
     return(TRUE)
   }
 
 
-  testDesign<-c(rep(1,n-b),specialNodes)
+  # Pick a starting design. Initiate the design so that 1s on all real nodes, and each block
+  # gets a special treatment
+  if (b>0){testDesign<-c(rep(1,n),c((p+1):(p+b)))} else {testDesign<-c(rep(1,n))}
 
-  ### Which rows are we intersted in for the block design- treatment rows, and block rows, assuming last one to be zero for each
-  #neworder<-c(1:(p),(p+b+p+1):(p+b+p+b))
-  #if (algorithm=="CE"){testDesign<-c(1,nextDesign(testDesign[1:(n-b-1)],p,algorithm="random"))}
-  if (algorithm=="CE"){testDesign<-c(1,sample(((1:(n-1))%%p+1),size=(n-1),replace=FALSE))} #Balances start
+  # For the CE algorithm, pck a random starting design instead
+  if (algorithm=="CE"){testDesign<-c(1,sample(((1:(n-1))%%p+1),size=(n-1),replace=FALSE))
+  AOptTest<-testDesign
+  if (b>0){testDesign<-c(testDesign,c((p+1):(p+b)))}
+  }
 
+  # For block designs, we generally set the treatment effect of the last treatment to zero, and
+  # set all the direct effects of the blocks to zero
+  # For doubly blocked designs, such as Latin Squares, we might want to also set some block effect
+  # such as the last to zero
   if (ignoreLastNode==FALSE){
-    neworder<-c(1:(p),(p+b+p+2):(p+b+p+b+1)) # Works for block designs
+    setToZero<-c((p+1):(2*p+b+1))
   }
   else
   {
-    neworder<-c(1:(p),(p+b+p+2):(p+b+p+b)) # Works for LS designs
+    setToZero<-c((p+1):(2*p+b+1),2*p+2*b+1)
   }
-  if (is.null(specialNodes)){
-    neworder<-c(1:p,(p+2):(2*p+1))
+
+  # For non-blocked designs, set the last treatment to be zero for identifiability.
+  if (is.null(blockList)){ # For non-blocked designs
+    setToZero<-p+1
+    optVars<-c(2:(p+1))
+    if (networkEffects==TRUE){optVars=c((p+2):(2*p+1))} # Do we want treatment effects, or network effects.
   }
-  #neworder<-c(1,2,3,13,14,16,17)# For 3x3 latin square
-  #neworder<-c(1,2,3,4,17,18,19,21,22,23)# For 3x3 latin square
-  #cat(neworder)
+
+### We now need to rewrite optVars to take account of those we set to zero,
+### i.e. if we want the second and 4th variable, and 3rd is set to zero,
+### We want the second and 3rd of the non-zero variables
+  binVars<-rep(0,2*p+2*b+1)
+  binVars[optVars]<-1
+  binVars<-binVars[-setToZero]
+  newOptVars<-(1:length(binVars))[as.logical(binVars)]
+
+  ### Now the main loop- keep going until we stop at some point
   stopCriterion<-FALSE
   while (stopCriterion==FALSE){
-
-    # if  (testLexSmallestFunction(testDesign[1:(n-b)])){
-    if  (testValidDesign(testDesign[1:(n-b)])){
-      #  print(testDesign)
-      # update GUI console
-      # flush.console()
-#      cat("\r",testDesign)
-      testWeight<-matrix(rep(0,(n*(p+b))),nrow=n)
-      # testWeight
-      for (j in 1:(n)){
+    if  (testValidDesign(testDesign[1:(n)])){
+      testWeight<-matrix(rep(0,((n+b)*(p+b))),nrow=(n+b))
+      for (j in 1:(n+b)){
         testWeight[j,testDesign[j]]<-1
       }
-      # for (j in (n+1):(n+b)){
-      #   testWeight[j,specialNodes[j-n+b]]<-1
-      # }
-      # print(testWeight)
-      #Test if we can find a new optimum
       infMatrix<-informationMatFull(A,testWeight)
-      # Gives a standard information matrix with rows corresponding to information about mean and then 2m parameters
-
-      infMatrix<-infMatrix[neworder,neworder] # Inf matrix for main and block effects
-
-      if ((determinant(infMatrix)$modulus)>-10){
-        # designEval$ATrial<- evaluateNetworkDesign(infMatrix,p)
+      # Gives a standard information matrix with rows corresponding to information about mean
+      # and then 2m parameters
+      infMatrix<-infMatrix[-setToZero,-setToZero] # Inf matrix reduced just for the non-zero effects
+      if ((determinant(infMatrix)$modulus)>-10){ # If the matrix is invertible
         numEval<-numEval+1;
         invInfMatrix<-solve(infMatrix,tol=1e-21)
-        #  invInfMatrix<-invInfMatrix[neworder,neworder] # Inf matrix for main and block effects
+
+        ##ATrial<-mean(diag(invInfMatrix)[newOptVars])  # This is A-optimality criteria, not pairwise.
+
+        ### A rather convoluted way to get the utility, but I can't think of anything faster...
         ATrial<-0
-        ATrialNet<-0
-
-        for (count1 in (2:p)){
-          for (count2 in (count1+1):(p+1)){
-            cVec<-rep(0,length(neworder))
-            cVec[count1]<-1
-            cVec[count2]<- -1
-            cVec[p+1]<-0
+        for (count1 in (1:(length(optVars)-1))){
+          for (count2 in (count1+1):(length(optVars))){
+            cVec<-rep(0,2*p+2*b+1)
+            cVec[optVars[count1]]<-1
+            cVec[optVars[count2]]<- -1
+            cVec<-cVec[-setToZero]
             ATrial<-ATrial+(cVec%*%invInfMatrix%*%(cVec))[1,1]
-
-            #  ATrial<-sum(diag(invInfMatrix)[2:p])
-             cVec2<-rep(0,2*p)
-             cVec2[count1+p-1]<- 1
-             cVec2[count2+p-1]<- -1
-            ATrialNet<-ATrialNet+(cVec2%*%invInfMatrix%*%(cVec2))[1,1]
-            #
-          }
         }
-
+        }
         ATrial<-ATrial/(p*(p-1)/2)
-        ATrialNet<-ATrialNet/(p*(p-1)/2)
 
-        if (ATrial<AOptVal){
+
+        if (ATrial<AOptVal){ # If we've found an optimal, store it and update the CE algorithm parameters
           AOptVal<-ATrial
           AOptDesign<-testWeight
-          successfulCE<-trialCE
-          numTrialsCE<-0}
-
-         if (ATrialNet<AOptNetVal){
-         AOptNetVal<-ATrialNet
-         AOptNetDesign<-testWeight
-         }
-
+          AOptTest<-testDesign
+          lastCEWin<-numTrialsCE
+          }
       }
-    }# End evaluate if canonical ordering
+    }# End evaluate if a valid design
 
-    #cat(testDesign,"\n")
-    numTrialsCE<-numTrialsCE+1
-    if ((algorithm=="CE") && (numTrialsCE%%(p+1)==0)){
-      testDesign<-c(nextDesign(testDesign[1:(n-b)],p,algorithm=algorithm,par=trialCE),specialNodes) # Put back to start of cycle
-      trialCE<-(trialCE+1)
-      if (trialCE>(n-b)){trialCE<-1}
-    }# If we've cycled through all designs for ExpUnit, advance to next.
+    numTrialsCE<-numTrialsCE+1 # How many trials have we had without success.
 
-    testDesign<-c(nextDesign(testDesign[1:(n-b)],p,algorithm=algorithm,par=trialCE),specialNodes)
+    ### Now pick the next design
+    if ((algorithm=="sequential") | (algorithm=="random")){
+     testDesign<-c(nextDesign(testDesign[1:n],p,algorithm=algorithm,par=trialCE))
+     }
 
-    if (length(testDesign)!=n){stopCriterion<-TRUE}
+     if (algorithm=="CE") {
+        testDesign<-nextCEDesign(AOptTest[1:n],p,numTrialsCE)
+    }
+
+    # Add on again the special block nodes which are fixed
+    if (b>0) {testDesign<-c(testDesign,c((p+1):(p+b)))}
+
+    # Stop if we've reached a stop criterion
+    if (length(testDesign)!=(n+b)){stopCriterion<-TRUE}
     if ((algorithm=="random") && (numEval == 100 )){stopCriterion<-TRUE}
-    if ((algorithm=="CE") && (numTrialsCE>(n-b)*p)){
+    if ((algorithm=="CE") && ((numTrialsCE-lastCEWin)>(n)*(p-1))){
       if (numCERandStarts>0){
-        testDesign<-c(1,nextDesign(testDesign[1:(n-b-1)],p,algorithm="random"),specialNodes)
+        testDesign<-c(1,nextDesign(testDesign[1:(n-1)],p,algorithm="random"),specialNodes)
         numCERandStarts<-numCERandStarts-1
       }
       else{
@@ -173,7 +181,19 @@ gridSearch<-function(A,p,isomorphisms=FALSE,specialNodes=NULL, ignoreLastNode=FA
       }
     }
   }
-#  cat("\n")
-  return(list("AOptVal"=AOptVal,"AOptDesign"=AOptDesign,"AOptNetVal"=AOptNetVal,"AOptNetDesign"=AOptDesign,"numEval"=numEval))
 
+  return(list("AOptVal"=AOptVal,"AOptDesign"=AOptDesign,"numEval"=numEval))
+  ##TODO: Consider replacing with (as.vector(AOptDesign%*%c(1:n)[1:n]))
+}
+
+### A utility function for CE. TODO: Incorporate in nextDesign
+nextCEDesign<-function(startDesign,p,CECount){
+  if (CECount<=0){return(startDesign)}
+  n<-length(startDesign)
+  startDesign<-startDesign-rep(1,n) # Take from 1->n notation to 0->n-1 for nicer modular arithmetic
+  varToChange<-(((CECount-1)%/%(p-1))%%n)+1  #
+  amountToChange<-(CECount-1)%%(p-1)+1
+  startDesign[varToChange]<-(startDesign[varToChange]+amountToChange)%%p
+
+  return(startDesign+rep(1,n))
 }
--- tests/testthat/test-gridSearch.R
+++ tests/testthat/test-gridSearch.R
@@ -1,3 +1,9 @@
-test_that("Simple Design Found", {
+test_that("Simple Design Found Example 1", {
   expect_equal(gridSearch(JRSSExamples$ex1,2)$AOptVal, 0.4186047, tolerance=1e-7)
 })
+test_that("Network Design Found Example 1", {
+  expect_equal(gridSearch(JRSSExamples$ex1,2, networkEffects = TRUE)$AOptVal, 0.2369146, tolerance=1e-7)
+
+})
+
+
